b497af817082295bf51772fbc9be652d932adf00
Marcus Francis <marcusmagnusson@outlook.com>
2020-09-05 01:17:32 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk');
+
+const chalks = {
+	error: chalk.red('error: '),
+	warning: chalk.yellow('warning: '),
+	ready: chalk.green('ready: '),
+	info: chalk.cyan('info: '),
+	event: chalk.magenta('event: ')
+};
+
+exports.log = {
+	event: (...message) => {
+		console.log(chalks.event, ...message);
+	},
+	error: (...message) => {
+		console.error(chalks.error, ...message);
+	},
+	warning: (...message) => {
+		console.warn(chalks.warning, ...message);
+	},
+	ready: (...message) => {
+		console.log(chalks.ready, ...message);
+	},
+	info: (...message) => {
+		console.log(chalks.info, ...message);
+	}
+};
